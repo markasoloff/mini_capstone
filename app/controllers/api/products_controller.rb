6a450484009e4b9c5936827f767ac992f5a7e5db
“Marksoloff” <marksoloff@gmail.com>
2018-08-28 19:28:13 -0500

    routes, controller, json.jbuilder drafts

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,5 @@
 class Api::ProductsController < ApplicationController
+  def all_action
+    @products
+    render all.json.jbuilder
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace api: do
+    get "/all_url" => 'api/products#all_action'
+  end
+end
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
-end
