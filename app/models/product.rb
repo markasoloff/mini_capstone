c020efa54540f09918a272e582401bce6f2dbb4f
“Marksoloff” <marksoloff@gmail.com>
2018-09-02 12:08:40 -0500

    partials

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,44 @@
 class Product < ApplicationRecord
+  def description_list
+    description.split(", ")
+    # 'description here acts as an attr. When applied to product.description, it will make that data dispaly as an array in browswer.'
+
+  end
+
+  def friendly_created_at
+    created_at.strftime("%m/%d/%Y")
+  end
+
+  def is_discounted?
+    price > 5000
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
+
+#the following method only applies to Josh's cookbook app
+  # def friendly_prep_time
+  #   prep_time/60
+  #   minutes = prep_time % 60
+  #   time_message = ""
+
+  #   if hours > 0
+  #     time_message +=  "{#{hours} Hours"
+
+  #   if hours > 0 && minutes > 0 
+  #       time_message += ", "
+  #   if minutes > 0
+  #     time_message += "#{minutes} Minutes"
+  #   end
+
+
+  #   "{#{hours} hours, #{minutes} minutes"
+  # end
+
+
---
+++ app/views/api/products/_product.json.jbuilder
@@ -0,0 +1,23 @@
+json.id product.id
+json.name product.name
+json.price product.price
+json.image_url product.image_url
+json.description product.description
+json.discounted product.is_discounted?
+
+if product.is_discounted?
+  json.sale_message "Discounted Price"
+end
+
+
+json.tax product.tax  
+json.total product.total
+
+
+  json.formatted do 
+    json.price number_to_currency(product.price)
+    json.tax number_to_currency(product.tax)
+    json.total number_to_currency(product.total)
+    
+  end
+
